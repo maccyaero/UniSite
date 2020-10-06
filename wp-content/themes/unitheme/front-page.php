b310544213dce3dde38a181bba28ece14dc14ede
Wali Abbas <waliabbas@ymail.com>
2020-10-06 22:27:50 +1100

    Manipulated the default URL based querry for the Events Archi page to only show the Events happening in the future.

--- wp-content/themes/unitheme/front-page.php
+++ wp-content/themes/unitheme/front-page.php
@@ -13,10 +13,24 @@
             <h3>Upcoming Events</h3>
 
             <?php 
+                $today = date('Ymd');
                 $homepageEvents = new WP_Query(
                     array(
                         'posts_per_page' => 2,
-                        'post_type' => 'uni_event'
+                        'post_type' => 'uni_event',
+                        'meta_key'=> 'event_date',
+                        'orderby' => 'meta_value_num',
+                        'order' => 'ASC',
+                        'meta_query' => array(
+                            array(
+                                'key' => 'event_date',
+                                'compare' => '>=',
+                                'value' => $today,
+                                'type' => 'numeric'
+                            )
+                        )
+
+
                     )
                 );
 
